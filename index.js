ccfbfb5328542bef4e168e4963870c01f058c2d4
Kolten Fluckiger <wrtunder@gmail.com>
2020-04-07 18:25:11 -0600

    Made changes to middleware instead of OR, AND for query, and params

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 class JSONMiddleware {
-  parse(req, res, next) {
-    if ((req.query === undefined) || (req.query.params === undefined)) {
-      return next()
-    };
-    const JSONQuery = {};
-    const query = typeof(req.query.params) === "string"
-      ? JSON.parse(req.query.params)
-      : req.query.params;
-    Object.keys(query).map(key => {
-      JSONQuery[key] = query[key];
-    })
-    req.query = JSONQuery;
-    return next();
-  }
+    parse(req, res, next) {
+        if ((req.query === undefined) && (req.query.params === undefined)) {
+            return next()
+        };
+        const JSONQuery = {};
+        Object.entries(req.query).forEach(([key, value]) => {
+            if (typeof(value) === 'string') {
+                const v = JSON.parse(value);
+                JSONQuery[key] = v;
+            } else {
+                JSONQuery[key] = value;
+            }
+        })
+        req.query = JSONQuery;
+        return next();
+    }
 }
 module.exports = new JSONMiddleware();
